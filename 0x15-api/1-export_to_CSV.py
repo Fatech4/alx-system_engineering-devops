5c610646ab9e181e574660820f745dc3f6b5c5f3
Faruk <adekoladamilola4@gmail.com>
2023-05-23 14:08:19 +0100

    Done with task 0 and 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+""" Write a Python script that, using this REST API, for a given
+employee ID, returns information about his/her TODO list progress.
+"""
+import urllib.request
+import json
+import sys
+import csv
+
+
+def fetch_employee_todo_progress(employee_id):
+    """ Function that implement the logic """
+    try:
+        # Fetch employee data
+        employee_url = "https://jsonplaceholder.typicode.com/users/{}"\
+                .format(employee_id)
+        employee_response = urllib.request.urlopen(employee_url)
+        employee_data = json.loads(employee_response.read())
+
+        # Fetch TODO data for the employee
+        todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos"\
+                    .format(employee_id)
+        todos_response = urllib.request.urlopen(todos_url)
+        todos_data = json.loads(todos_response.read())
+
+        # Extract relevant information
+        employee_name = employee_data["name"]
+        total_tasks = len(todos_data)
+        done_tasks = [task["title"] for task in todos_data if
+                      task["completed"]]
+
+        # Export to CSV
+        csv_filename = "{}.csv".format(employee_id)
+        with open(csv_filename, mode="w", newline="") as csv_file:
+            writer = csv.writer(
+                    csv_file, quotechar='"', quoting=csv.QUOTE_ALL)
+            for task in todos_data:
+                writer.writerow(
+                                [employee_id, employee_name,
+                                    task['completed'],
+                                    task['title']])
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+    employee_id = int(sys.argv[1])
+    fetch_employee_todo_progress(employee_id)

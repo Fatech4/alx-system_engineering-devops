b24e7ff3d744fd9820b55a2acc3eb2ca1178c930
Faruk <adekoladamilola4@gmail.com>
2023-06-07 20:18:45 +0100

    Done with task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -17,25 +17,28 @@ def recurse(subreddit, after=None, hot_list=None):
         hot_list = []
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     params = {
-            'after': after
+            "limit": 100,
+            "raw_json": 1
     }
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
         AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110\
         Safari/537.3"
     }
-    response = requests.get(url, params=params, headers=headers)
+    response = requests.get(url, params=params, headers=headers,
+                            allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        query = data['data']['after']
-        if data.get(query):
-            after = data['data']['after']
-            posts = (data['data']['children'])
+        posts = (data['data']['children'])
+        if not posts:
+            return hot_list
 
-            for post in posts:
-                hot_list.append(post['data']['title'])
-
-            return recurse(subreddit, after, hot_list)
-        return (hot_list)
+        for post in posts:
+            hot_list.append(post['data']['title'])
+        after = data['data']['after']
+        if after:
+            return recurse(subreddit, after=after, hot_list=hot_list)
+        else:
+            return (hot_list)
     else:
         return (None)

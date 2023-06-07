198c71c9cd59bbf7c330b97c1514b68127ac4ad5
Faruk <adekoladamilola4@gmail.com>
2023-06-07 16:02:15 +0100

    refix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """ A module that contains a function that uses the Reddit API to retrieve
 the number of subscribers for a given subreddit:
-    """
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
     """ function that uses the Reddit API to retrieve the number of
-    subscribers for a given subreddit:
+    subscribers for a given subreddit
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
         AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110\

482db7b44827c3db9250c686c86d6c203ee64e61
Faruk <adekoladamilola4@gmail.com>
2023-06-08 19:05:54 +0100

    fixes

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 #solution to web stack debugging using puppet
 exec { 'fix-wordpress':
 path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-command => 'sed -t -e "s/phpp/php/g" /var/www/html/wp-settings.php',
+command => 'sed -i -e "s/phpp/php/g" /var/www/html/wp-settings.php',
 provide => 'shell',
 }

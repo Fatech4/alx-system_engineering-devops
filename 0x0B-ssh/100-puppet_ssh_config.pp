b4c4b7e94ea3e5a0bb68def037f55b6750b39e7b
Faruk <adekoladamilola4@gmail.com>
2023-06-16 07:48:09 +0100

    fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,20 +1,30 @@
-# Client configuration file (w/ Puppet)
+# Client configuration file (w/ Puppet}
+package { 'augeas-tools':
+  ensure => installed,
+}
+
 file { '/etc/ssh/sshd_config':
   ensure  => file,
   owner   => 'root',
   group   => 'root',
   mode    => '0644',
-  content => "
-    # SSH client configuration
-    Host *
-      IdentityFile ~/.ssh/school
-      PasswordAuthentication no
-  ",
   notify  => Service['ssh'],
+  require => Package['augeas-tools'],
+  before  => Service['ssh'],
+}
+
+augeas { 'sshd_config':
+  context => '/files/etc/ssh/sshd_config',
+  changes => [
+    'set * IdentityFile ~/.ssh/school',
+    'set * PasswordAuthentication no',
+  ],
+  require => File['/etc/ssh/sshd_config'],
 }
 
 service { 'ssh':
   ensure  => running,
   enable  => true,
-  require => File['/etc/ssh/sshd_config'],
+  require => Augeas['sshd_config'],
 }
+
